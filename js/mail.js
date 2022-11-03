de23ad02c4891ba6ba740c5866ecd494b4d90f9e
ippoRulez <ippolitodonato@gmail.com>
2022-11-03 20:48:56 +0100

    added more styling and logic to the email exercise

--- js/mail.js
+++ js/mail.js
@@ -1,23 +1,35 @@
+const submitBtnEl = document.getElementById('submit');
+const failureEl = document.getElementById('failure');
+const successEl = document.getElementById('success');
+
 const mailAllowed = [
 	'giovannirossi@mail.it',
 	'mariobianchi@mail.it',
 	'giginopeluzzo@mail.it',
 ];
 
-const userMail = prompt('Inserisci la tua email');
+const userMailEl = document.querySelector('[type = email]');
 
-let found = false;
+submitBtnEl.addEventListener('click', function () {
+	const userMail = userMailEl.value;
 
-for (let i = 0; i < mailAllowed.length; i++) {
-	const element = mailAllowed[i];
+	let found = false;
 
-	if (element === userMail) {
-		found = true;
+	for (let i = 0; i < mailAllowed.length; i++) {
+		const element = mailAllowed[i];
+
+		if (element === userMail) {
+			found = true;
+		}
 	}
-}
 
-if (found) {
-	alert('Utente autorizzato. Benvenuto.');
-} else {
-	alert('Utente non autorizzato.');
-}
+	console.log(found);
+
+	if (found) {
+		successEl.classList.remove('d-none');
+		failureEl.classList.add('d-none');
+	} else {
+		successEl.classList.add('d-none');
+		failureEl.classList.remove('d-none');
+	}
+});
